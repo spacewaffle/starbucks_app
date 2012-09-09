cc22e7f40dcee2867b280fd4edf2324f52272d9f
JonCheng <jon.syndicate@gmail.com>
2012-09-09 16:43:08 -0400

    initiall commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StarbucksApp::Application.config.secret_token = '870f2194fb953a9419358e4040c0b08a6b1f7cc3714e3039e3134d5a1e489f79749c865c5f592cee4278afee931f35d9168821d3b0dc595c6cef86578eca0ae4'
